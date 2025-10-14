3e11eb9a511d11081ba765e6ac30b1244b4b51d0
guisbeghendev <guisbeghendev@gmail.com>
2025-10-14 18:54:41 -0300

    atualização geral

--- AgroData/wsgi.py
+++ AgroData/wsgi.py
@@ -8,6 +8,13 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+# CORREÇÃO: ADICIONA O CAMINHO DO VIRTUAL ENVIRONMENT (VENV) AO PYTHON PATH
+# O mod_wsgi está falhando ao reconhecer o python-home do Apache.
+# Forçar o caminho aqui garante que o módulo 'django' e 'AgroData' sejam encontrados.
+sys.path.insert(0, '/var/www/univesp/venv/lib/python3.10/site-packages')
+sys.path.insert(0, '/var/www/univesp')
 
 from django.core.wsgi import get_wsgi_application
 
