b33edfff87f4a497cfbe9cd60f5751d5915a71d5
guisbeghendev <guisbeghendev@gmail.com>
2025-10-15 11:25:03 -0300

    atualização geral

--- home/forms.py
+++ home/forms.py
@@ -1,11 +1,17 @@
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 
+
 class CustomUserCreationForm(UserCreationForm):
+    # O UserCreationForm já trata dos campos username, password, password2
+
     class Meta(UserCreationForm.Meta):
+        # A forma mais segura de listar os campos ao herdar
+        fields = ('username', 'password', 'password2')
+
         labels = {
             'username': 'Nome de Usuário',
-            # 'password': 'Senha', # Correto: Removido para evitar a falha de renderização.
+            # Deixe o label da senha FORA, pois ele é a causa do erro
             'password2': 'Confirme a Senha',
         }
         help_texts = {
